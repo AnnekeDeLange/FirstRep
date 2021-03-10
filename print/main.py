bafa6a0cc1f2e56366cf3e47176ce037c03cfa55
Anneke de Lange <44265839+AnnekeDeLange@users.noreply.github.com>
2021-03-10 18:38:48 +0100

    Add files via upload

---
+++ print/main.py
@@ -0,0 +1,12 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print ('zover zijn we alvast gekomen')
+print ('zover ', 'zijn ', 'we ', 'met ', 'losse elementen ', 'ook ', 'al!')
+print (1,2,3)
+tango = 'een twee drie vier'
+print(tango, 'hoedje van papier')
+print(5, 6, end=' *')
+print(7, 8, end='\n')
