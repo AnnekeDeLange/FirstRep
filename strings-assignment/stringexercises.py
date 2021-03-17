261d281ca71dd527be97f10a489ce134d4bc4dab
AnnekeDeLange <44265839+AnnekeDeLange@users.noreply.github.com>
2021-03-17 10:51:48 +0100

    Add files via upload
    Added folders and files of BED exercises and assignments

---
+++ strings-assignment/stringexercises.py
@@ -0,0 +1,24 @@
+# str1 = "Deze is simpel"
+# str2 = "Deze is ook simpel, maar met een nieuwe regel \n"
+# str3 = ('simpel \\\n')
+# str4 = ('Met tabs \t'+'en nog een tab \t \n')
+# str5 = ('begin \n\teen \n\ttwee\n \tdrie \neind')
+# print(str5)
+# print('begin \n \t een \n \t twee \n \t drie \n eind \n')
+# print('This line contains a \' \n')
+# print('\a')
+# print ('\u0398')
+# print('\u1683')
+# print('\u022F')
+# print('\U00001638')
+# print('1 en \v2 \n')
+# print('\u2192 \N{rightwards arrow}')
+# print('a \fb')
+# print('\u00AE \N{Registered sign}')
+# print("""Een heel verhaal
+# over meerdere regels
+# en nog meer""")
+result = str(5) in 'We were lucky that they had a table for 5.'
+print(result)
+result2 = ('me' ==  'me')
+print(result2)
